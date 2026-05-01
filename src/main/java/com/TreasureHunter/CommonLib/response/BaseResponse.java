c3822c0a977033b42d66efe4552f2498f35748e2
nguyen thu <nguyenthu180100@gmail.com>
2026-05-01 15:38:03 +0700

    fix base response

--- src/main/java/com/TreasureHunter/CommonLib/response/BaseResponse.java
+++ src/main/java/com/TreasureHunter/CommonLib/response/BaseResponse.java
@@ -1,13 +1,19 @@
 package com.TreasureHunter.CommonLib.response;
 
 import com.TreasureHunter.CommonLib.util.TraceContextUtil;
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import lombok.AllArgsConstructor;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 import lombok.Setter;
 
 import java.time.Instant;
 
 @Getter
 @Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@JsonIgnoreProperties(ignoreUnknown = true)
 public class BaseResponse<T> {
     private String code;
     private String message;
